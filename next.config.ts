de43ce650de740fbefe472a2c08c32f5db6f3443
Manjeet Shinde <manjeetshinde1996@gmail.com>
2025-04-14 20:17:01 +0530

    Update Next.js configuration for basePath and improve GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "mms-portfolio",
+  basePath: "/portfolio",
   trailingSlash: true,
   images: {
     unoptimized: true,
