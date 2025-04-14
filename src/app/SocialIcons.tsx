afa3d30f2dac57feaad865f0379e6ed7b7f69006
Manjeet Shinde <manjeetshinde1996@gmail.com>
2025-04-14 20:25:44 +0530

    Add title attribute to Link in SocialIcons component for improved accessibility

--- src/app/SocialIcons.tsx
+++ src/app/SocialIcons.tsx
@@ -9,7 +9,7 @@ interface SocialIconsProps {
 
 export const SocialIcons = ({ name, url, icon: Icon }: SocialIconsProps) => {
   return (
-    <Link href={url} target="_blank">
+    <Link href={url} target="_blank" title={name}>
       <div className="rounded-2xl border-2 border-gray-600 p-4 hover:bg-black hover:text-white">
         <Icon className="text-4xl" />
       </div>
