d3b1661793c6f1dd2a2f63a9371acf58a222af2a
Manjeet Shinde <manjeet.shinde@outlook.com>
2025-07-06 17:00:31 +0530

    Refactor: Update package.json and pnpm-lock.yaml with new dependencies, enhance globals.css for improved styling, and implement CardContainer component for 3D card effects in Introduction component.

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import Card from "./Card";
+import { CardContainer } from "./ui/3d-card";
 
 export default function Introduction() {
   return (
-    <Card>
-      <Image
-        src="/manjeet-shinde.jpg"
-        width={400} // or your image width
-        height={400} // or your image height
-        className="m-auto w-full rounded-2xl grayscale hover:grayscale-0"
-        alt="Manjeet profile image"
-        priority
-      />
-      <Card className="gap-2">
-        <h1 className="text-2xl font-semibold">Manjeet Shinde</h1>
-        <p>Developer | Founder@Procillage</p>
+    <CardContainer>
+      <Card>
+        <Image
+          src="/manjeet-shinde.jpg"
+          width={400} // or your image width
+          height={400} // or your image height
+          className="m-auto w-full rounded-2xl grayscale hover:grayscale-0"
+          alt="Manjeet profile image"
+          priority
+        />
+        <Card className="gap-2">
+          <h1 className="text-2xl font-semibold">Manjeet Shinde</h1>
+          <p>Developer | Founder@Procillage</p>
+        </Card>
       </Card>
-    </Card>
+    </CardContainer>
   );
 }
